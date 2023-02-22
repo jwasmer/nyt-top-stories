39e16fd9affb240b04ce089a10c3c410c64975ff
James Wasmer <jameswasmer@gmail.com>
2023-02-22 18:38:43 -0500

    Extract and display article data from useNavigate

--- src/Components/DetailView.js
+++ src/Components/DetailView.js
@@ -1,5 +1,21 @@
+import { useLocation, Link } from "react-router-dom"
+
 export default function DetailView() {
+  const location = useLocation()
+  const article = location.state.content
+
+  const getDate = (rawDate) => {
+    const date = new Date(rawDate)
+
+    return date.toLocaleDateString()
+  }
+
   return (
-    <div>DetailView</div>
+    <div>
+      <h4>{article.title}</h4>
+      <h5>{article.abstract}</h5>
+      <p>{article.byline}, {getDate(article.created_date)}</p>
+      <Link to={article.short_url}>Read the full story here.</Link>
+    </div>
   )
 }
