6953184f334bceac1a0db4bc9357ec06ef53d453
James Wasmer <jameswasmer@gmail.com>
2023-02-22 14:25:36 -0500

    Call NYT api and display results

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import { getData } from './api-calls';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const apiKey = process.env.REACT_APP_NYT_KEY
+
+  const [data, setData] = useState(null)
+  const [section, setSection] = useState('home')
+  const [url, setUrl] = useState(`https://api.nytimes.com/svc/topstories/v2/${section}.json?api-key=${apiKey}`)
+
+  useEffect(() => {
+    console.log(url)
+    getData(url)
+      .then(data => {
+        setData(data)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }, [])
+
+  const buildHeadlineCards = (data) => {
+    data.results.map(result => {
+      <div>
+        {result.title}
+      </div>
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {data && buildHeadlineCards(data)}
+    </>
   );
 }
 
