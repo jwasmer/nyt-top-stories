1a56d0b9c8cf85238298e8acb840829b62523f70
James Wasmer <jameswasmer@gmail.com>
2023-02-22 14:46:40 -0500

    Return title cards from function when built

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import { useState, useEffect } from 'react';
 
 function App() {
   const apiKey = process.env.REACT_APP_NYT_KEY
+  console.log(process.env.REACT_APP_NYT_KEY)
 
   const [data, setData] = useState(null)
   const [section, setSection] = useState('home')
   const [url, setUrl] = useState(`https://api.nytimes.com/svc/topstories/v2/${section}.json?api-key=${apiKey}`)
 
   useEffect(() => {
-    console.log(url)
     getData(url)
       .then(data => {
         setData(data)
@@ -21,10 +21,13 @@ function App() {
   }, [])
 
   const buildHeadlineCards = (data) => {
-    data.results.map(result => {
+    console.log('buildHeadlineCards fired')
+    return data.results.map(result => {
+      return (
       <div>
         {result.title}
       </div>
+      )
     })
   }
 
